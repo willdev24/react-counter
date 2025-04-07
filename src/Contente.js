7ccce544ce2d168114aa700a6d562708f01ecaf1
willy thiago <128657856+willdev24@users.noreply.github.com>
2025-04-07 14:59:10 -0400

    ok

--- src/Contente.js
+++ src/Contente.js
@@ -2,22 +2,27 @@ import React, {useState} from "react";
 
 import "./Comtent.css"
 
-
+let cont = 0
 const Couter = ({titulo})=>{
  
-    
 const [contador, setContador] = useState(0)
 
+function adicao(id){
+ 
+    if(id == 0){
+        cont = cont + 1
+       const dados = cont
+        setContador(dados)
+    }
 
-function menos(){
-    setContador(452)
-  
-     }
-     
-     function mais(){
-    setContador(contador)
-     
-     }
+    if(id == 1){
+        cont = cont - 1
+       const dados = cont
+       if(dados <= 0 ) 
+        setContador(dados)
+    }
+    
+}
 
     return(
     <div className="contador"> 
@@ -25,16 +30,16 @@ function menos(){
         <span>{contador}</span>
        
        <article className="mm"> 
-        <button onClick={()=>mais()}>+</button>
-        <button onClick={()=>menos()}>-</button>
+        <button onClick={()=>adicao(0)}>+</button>
+        <button onClick={()=>adicao(1)}>-</button>
        </article>
        
     </div>
 
     )
 
-
 }
+
 
 
 
